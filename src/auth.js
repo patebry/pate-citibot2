a62edb660dfdedb74b3912d995ebbc9de6b84b30
Pate Byant <patebryant@Pates-MacBook-Pro.local>
2017-11-07 15:00:39 -0500

    added README instructions

--- src/auth.js
+++ src/auth.js
@@ -1,12 +1,13 @@
 import auth0 from 'auth0-js'
 import history from './history'
+import { AUTH_CONFIG } from './auth0-variables'
 
 export default class Auth {
 	auth0 = new auth0.WebAuth({
-		domain: 'patebry1.auth0.com',
-		clientID: 'OHj48zFRpyPMMvHlvMu7z1tqVSY3DJAz',
-		redirectUri: 'http://localhost:3000/callback',
-		audience: 'https://patebry1.auth0.com/api/v2/',
+		domain: AUTH_CONFIG.domain,
+		clientID: AUTH_CONFIG.clientId,
+		redirectUri: AUTH_CONFIG.callbackUrl,
+		audience: `https://${AUTH_CONFIG.domain}/userinfo`,
 		responseType: 'token id_token',
 		scope: 'openid'
 	})
---
+++ src/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'patebry1.auth0.com',
+  clientId: 'OHj48zFRpyPMMvHlvMu7z1tqVSY3DJAz',
+  callbackUrl: 'http://localhost:3000/callback'
+}
