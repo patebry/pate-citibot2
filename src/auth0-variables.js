b37a5456800aa2d392dd224b70e42574be8cb465
Pate Byant <patebryant@Pates-MacBook-Pro.local>
2017-11-07 16:04:28 -0500

    auth0-variables changed

--- src/auth0-variables.js
+++ src/auth0-variables.js
@@ -1,5 +1,12 @@
-export const AUTH_CONFIG = {
+/*export const AUTH_CONFIG = {
   domain: 'patebry1.auth0.com',
   clientId: 'OHj48zFRpyPMMvHlvMu7z1tqVSY3DJAz',
   callbackUrl: 'http://localhost:3000/callback'
+}
+*/
+
+export const AUTH_CONFIG = {
+	domain: '',
+	clientId: '',
+	callbackUrl: ''
 }
